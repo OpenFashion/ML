d900f1a16689132a13e30e84045f82e5929863d1
abalaki6 <arxartyom@gmail.com>
2018-02-25 03:01:22 -0600

    all done but parallelism

--- mlfashion.py
+++ mlfashion.py
@@ -1,8 +1,10 @@
 import numpy as np
 import tensorflow as tf
 
+from models.CNN import CNN
 from open_fashion import train_segmentation
-from texture_mapper import plug_texture
+from texture_mapper import plug_texture, get_segmented_cloath
+from data.preprocessing import *
 
 
 def labels():
@@ -15,7 +17,25 @@ def train(datafolder, datafile, modelfolder):
 		@param modelfolder {string} the path to folder where models should be stored
 		@return dict{} dictionary of models for each label in mlfashion.labels()
 	'''
-	pass
+	models = {}
+	X,Y,_ = get_data_info(datafolder, datafile)
+	
+	# read input tensor
+	X = get_tensor(X)
+	for label in labels():
+		models[label] = CNN(label)
+		# create separate thread or process
+		
+		# get output tensor
+		T = get_output_tensor(Y, label)
+		train_segmentation(models[label], X, T, modelfolder)
+		models[label].save(modelfolder + '/safesave/' + label + '.ckpt')
+		models[label].save(modelfolder + label + '.ckpt')
+		del T
+
+		# end of part in parallel
+
+	return models
 	
 def texture_swap(texture, cloath, models, original_image):
 	'''
@@ -25,11 +45,15 @@ def texture_swap(texture, cloath, models, original_image):
 		@param original_image {image\tensor3d\numpy3d`} the original image that we want to change
 		@return {image} image with replaced cloath with texture
 	'''
-	pass	
+	segment = get_segmented_cloath(models[cloath], original_image, cloath)
+	return plug_texture(texture, cloath, original_image, segment)
 
-def loadmodels(modelsfolder):
+def load_models(modelsfolder):
 	'''
 		@param modelfolder the path to the folder where models are saved
 		@return dict{} dictionary of the models for each label in mlfashion.labels()
 	'''
-	pass
+	models = {}
+	for label in labels():
+		models[label] = CNN.load_model(modelfolder + label + '.ckpt')
+	return models
